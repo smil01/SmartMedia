8aec1ea237b33d76efb88dc35fdc1ac47e392897
pc-22 <pc-22@pc-22-PC>
2019-04-24 15:16:09 +0900

    자바 수업 프로젝트 첫생성

---
+++ Ex0424/src/Ex01.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author 오대근
+ * @content 첫수업
+ *
+ */
+public class Ex01 {
+	// public 접근제한자
+	// class : class 시작
+	// Ex01 클래스의 이름, 파일명과 동일해야함
+
+	public static void main(String[] args) {
+		// method, 함수
+	}
+
+}
