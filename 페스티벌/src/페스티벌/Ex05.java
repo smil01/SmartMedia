370b6290d4c1e34afc4ca9f1f33ac44d52074865
오대근 <@pc-22-PC>
2019-04-30 15:37:12 +0900

    5번째

--- 페스티벌/src/페스티벌/Ex05.java
+++ 페스티벌/src/페스티벌/Ex05.java
@@ -4,11 +4,11 @@ public class Ex05 {
 
 	public static String change124(int n) { // 함수를 만들때 static을 꼭 써야지 main에서 호출가능 이유가 알고싶다면 저에게 오세요.
 		String answer = "";
-        int[] arr = {4, 1, 2};
-        int a;
+        int[] arr = {4, 1, 2}; // 반복수
+        int a; // 중복해서 쓸 수
         while (n > 0) {
-            a = n % 3;
-            n = n / 3;
+            a = n % 3; // 3으로 나누고 나머지를 첫번째 숫자로 씀
+            n = n / 3; // 그 다음 자리수 결정을 위한 중복수 처리과정
             if (a == 0) {
                 n -= 1;
             }
